abfedc194b7e3190406d50a0d40a03ddf96a7938
Vilmar Nepomuceno <vilmarnepomuceno@recife.ifpe.edu.br>
2025-05-26 11:45:17 -0300

    Aula 26_05

--- src/main/java/br/edu/ifpe/dsc/controlador/HelloWorldRestController.java
+++ src/main/java/br/edu/ifpe/dsc/controlador/HelloWorldRestController.java
@@ -1,14 +1,35 @@
 package br.edu.ifpe.dsc.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/hello")
 public class HelloWorldRestController {
 
-    @GetMapping("/")
+    @GetMapping
     public String hellooWorld() {
         return "Hello World!!";
+    }
+
+    @GetMapping("/param")
+    public String hellooWorldParam(@RequestParam("nome") String nome, @RequestParam("sobrenome") String sobrenome, 
+        @RequestParam("valor") String valor) {
+            
+        return "Hello World, " + nome + " " + sobrenome+ "!!";
+    }
+
+    @GetMapping("/path/{nome}/{sobrenome}")
+    public String hellooWorldPath(@PathVariable String nome, @PathVariable String sobrenome) {
+        return "Hello World, " + nome + " " + sobrenome+ "!!";
+    }
+
+    @GetMapping("/path/{nome}/{sobrenome}/{valor}")
+    public String hellooWorldPath2(@PathVariable String nome, @PathVariable String sobrenome, @PathVariable String valor) {
+        return "Hello World, " + nome + " " + sobrenome+ "!!" + " - Valor: " + valor;
     }
 
 }
