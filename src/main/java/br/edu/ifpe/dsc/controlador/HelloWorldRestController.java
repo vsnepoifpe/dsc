b7255400aab01b357c6b0a5bcb32f47f2c323f95
Vilmar Nepomuceno <vilmarnepomuceno@recife.ifpe.edu.br>
2025-05-30 09:58:29 -0300

    Aula 30/05

--- src/main/java/br/edu/ifpe/dsc/controlador/HelloWorldRestController.java
+++ src/main/java/br/edu/ifpe/dsc/controlador/HelloWorldRestController.java
@@ -2,9 +2,12 @@ package br.edu.ifpe.dsc.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifpe.dsc.model.dto.Usuario;
 
 @RestController
 @RequestMapping("/api/hello")
@@ -16,7 +19,8 @@ public class HelloWorldRestController {
     }
 
     @GetMapping("/param")
-    public String hellooWorldParam(@RequestParam("nome") String nome, @RequestParam("sobrenome") String sobrenome, 
+    public String hellooWorldParam(@RequestParam("nome") String nome, 
+        @RequestParam("sobrenome") String sobrenome, 
         @RequestParam("valor") String valor) {
             
         return "Hello World, " + nome + " " + sobrenome+ "!!";
@@ -30,6 +34,16 @@ public class HelloWorldRestController {
     @GetMapping("/path/{nome}/{sobrenome}/{valor}")
     public String hellooWorldPath2(@PathVariable String nome, @PathVariable String sobrenome, @PathVariable String valor) {
         return "Hello World, " + nome + " " + sobrenome+ "!!" + " - Valor: " + valor;
+    }
+
+    @GetMapping("/usuario")
+    public String hellooWorldJson(@RequestBody Usuario usuario) {
+        return "Hello World, " + usuario.getNome() + " " + usuario.getSobrenome()+ "!!";
+    }
+
+    @GetMapping("/teste")
+    public String hellooWorldJson2(@RequestBody String json) {
+        return "Hello World, " + json + "!!";
     }
 
 }
---
+++ src/main/java/br/edu/ifpe/dsc/model/dto/Usuario.java
@@ -0,0 +1,47 @@
+package br.edu.ifpe.dsc.model.dto;
+
+public class Usuario {
+
+
+    private String nome;
+    private String sobrenome;   
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [nome=" + nome + ", sobrenome=" + sobrenome + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+        return result;
+    }
+
+}
